6d39151f5de689c02d7ca1c3b9aa49ba7403e69c
ARKOMOL <80365662+ARKOMOL@users.noreply.github.com>
2023-01-18 00:07:36 +0600

    change

--- src/Components/Country.js
+++ src/Components/Country.js
@@ -13,13 +13,7 @@ const Country = ({country}) => {
           <h1>
          {timezones}
           </h1> 
-          <h1>
-         {idd.root}
-          </h1>
-          <h1>
-         {idd.root}
-          </h1>
-          <h1>
+            <h1>
          {idd.root}
           </h1>
           <img src={flags.png} alt="" />
