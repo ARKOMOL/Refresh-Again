ef30d6c4e70709c1f94b8baf5b99c03262deb23b
ARKOMOL <80365662+ARKOMOL@users.noreply.github.com>
2023-01-21 21:03:26 +0600

    h

--- src/Components/Country.js
+++ src/Components/Country.js
@@ -17,9 +17,7 @@ const Country = ({country}) => {
           <h1>
          {timezones}
           </h1> 
-          <h1>
-         {timezones}
-          </h1> 
+        
             <h1>
          {idd.root}
           </h1>
