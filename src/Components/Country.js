188e73af1c0da302deb71b1044efbab435d687d9
ARKOMOL <80365662+ARKOMOL@users.noreply.github.com>
2023-01-30 23:13:13 +0600

    h

--- src/Components/Country.js
+++ src/Components/Country.js
@@ -25,11 +25,7 @@ const Country = ({country}) => {
           <h1>
          {idd.root}
           </h1>
-          
-          <h1>
-         {idd.root}
-          </h1>
-           
+        
           
           <img src={flags.png} alt="" />
           
