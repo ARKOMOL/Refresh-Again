42818df056332f2863bb5d1b003cc63f599c0667
ARKOMOL <80365662+ARKOMOL@users.noreply.github.com>
2023-03-06 23:12:51 +0600

    ks

--- src/Components/Country.js
+++ src/Components/Country.js
@@ -29,9 +29,6 @@ const Country = ({country}) => {
              <h1>
          {idd.root}
           </h1>       
-                 <h1>
-         {idd.root}
-          </h1>   
               
             
         
